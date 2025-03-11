2fddc6af54e5e38a0178642ecb61082921477dc5
Anish Awasthi <anishawasthi16@gmail.com>
2025-03-11 23:41:25 +0530

    init mono repo and cleint- react app with tailwind and shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-amber-100 text-blue-500 flex w-screen h-screen items-center justify-center">
+      Testing 
+      <Button>Tailwind</Button>
+    </div>
+  );
+}
+
+export default App;
